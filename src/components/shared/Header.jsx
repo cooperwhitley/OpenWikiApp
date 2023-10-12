ab2a12d874b1ab3b75cfa1d411d89bf4e7ad173d
cooperwhitley <whitleycooperr@gmail.com>
2023-10-12 14:35:54 -0500

    Add link to index, change to darker style

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -39,11 +39,16 @@ const alwaysOptions = (
 				Home
 			</Link>
 		</Nav.Link>
+		<Nav.Link>
+			<Link to='/articles' style={linkStyle}>
+				Index
+			</Link>
+		</Nav.Link>
 	</>
 )
 
 const Header = ({ user }) => (
-	<Navbar bg='primary' variant='dark' expand='md'>
+	<Navbar bg='dark' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle} className='m-2'>
                 OpenWiki
