2aa312e848e695b26f0b806ebf4622785dae32e1
cooperwhitley <whitleycooperr@gmail.com>
2023-10-13 14:10:38 -0500

    add basic read ability

--- src/components/articles/ArticleShow.jsx
+++ src/components/articles/ArticleShow.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import LoadingScreen from '../shared/LoadingScreen'
-import { Container, Card, Button } from 'react-bootstrap'
+import { Container, Card, Button, Row, Col } from 'react-bootstrap'
 
 import { getOneArticle, updateArticle, removeArticle } from '../../api/article'
 
-import messages, { showArticleFailure } from '../shared/AutoDismissAlert/messages'
+import messages from '../shared/AutoDismissAlert/messages'
 
-export default function ArticleShow ({ user, msgAlert }) {
+export default function ArticleShow (props) {
     const [article, setArticle] = useState(null)
     
     const [updated, setUpdated] = useState(false)
@@ -15,6 +15,7 @@ export default function ArticleShow ({ user, msgAlert }) {
     const navigate = useNavigate()
 
     const { id } = useParams()
+    const {user, msgAlert} = props
     
     useEffect(() => {
         getOneArticle(id)
@@ -22,15 +23,47 @@ export default function ArticleShow ({ user, msgAlert }) {
             .catch(err => {
                 msgAlert({
                     heading: 'Error',
-                    message: showArticleFailure,
+                    message: messages.showArticleFailure,
                     variant: 'danger'
                 })
             })
     }, [updated])
+
+    let infoBoxes
+    if (article && article.infoBoxes.length > 0) {
+        infoBoxes = article.infoBoxes.map(infoBox => (
+            <Card.Text>
+                <Row>
+                    <Col style={{borderRight: '1px solid black'}}>{infoBox.title}</Col>
+                    <Col>{infoBox.summary}</Col>
+                </Row>
+            </Card.Text>
+        ))
+    }
+
+    if (!article) {
+        return <LoadingScreen />
+    }
     
+
     return (
-        <div>
-            <h1>{article.title}</h1>
-        </div>
+        <>
+            <Container className='m-2'>
+                <Row>
+                    <Col>
+                        <h1>{article.title}</h1>
+                        <p>{article.summary}</p>
+                    </Col>
+                    <Col sm='4'>
+                        <Card>
+                            <Card.Img variant="top" src={article.photo}/>
+                            <Card.Body>
+                                {infoBoxes}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
