fce33b4cc393012d75d19ad980199f4673240bf4
cooperwhitley <whitleycooperr@gmail.com>
2023-10-16 10:20:23 -0500

    update home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,10 @@ const Home = (props) => {
 	console.log('props in home', props)
 
 	return (
-		<>
-			<h2>Home Page</h2>
-		</>
+		<div style={{display: 'flex', flexDirection: 'column', alignContent: 'center'}}>
+			<h2>Welcome to OpenWiki</h2>
+			<h4>Check out the index, or sign up to add to the knowledge base!</h4>
+		</div>
 	)
 }
 
