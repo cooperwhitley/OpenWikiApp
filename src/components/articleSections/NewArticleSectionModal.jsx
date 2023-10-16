52407f4793da1ec7cc4fa2d443641d9736dc26b6
cooperwhitley <whitleycooperr@gmail.com>
2023-10-16 00:39:59 -0500

    Derive from edit article section modal

---
+++ src/components/articleSections/NewArticleSectionModal.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
+import ArticleSectionForm from '../shared/forms/ArticleSectionForm'
+import messages from '../shared/AutoDismissAlert/messages'
+import { createArticleSection} from '../../api/articleSection'
+
+export default function NewArticleSectionModal(props) {
+    const { user, article, show, handleClose, msgAlert, triggerRefresh } = props
+    const [section, setSection] = useState({})
+
+    const onChange = (evt) => {
+        evt.persist()
+
+        setSection(prevSection => {
+            const updatedName = evt.target.name
+            let updatedValue = evt.target.value
+
+            const updatedSection = { [updatedName] : updatedValue }
+
+            return {
+                ...prevSection, ...updatedSection
+            }
+        })
+    }
+
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        console.log(user)
+        createArticleSection(user, article._id, section)
+            .then(() => handleClose())
+            .then(() => {
+                msgAlert({
+                    heading: 'success',
+                    message: messages.createArticleSectionSuccess,
+                    variant: 'success'
+                })
+            })
+            .then(() => triggerRefresh())
+            .catch(() => {
+                msgAlert({
+                    heading: 'error',
+                    message: messages.createArticleSectionFailure,
+                    variant: 'danger'
+                })
+            })
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton />
+            <Modal.Body>
+                <ArticleSectionForm
+                    section={section}
+                    handleChange={onChange}
+                    handleSubmit={onSubmit}
+                    heading='Create article section'
+                />
+            </Modal.Body>
+        </Modal>
+    )
+}
