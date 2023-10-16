a14dfc14e83da30b14df49d2844fa81152b1ae89
cooperwhitley <whitleycooperr@gmail.com>
2023-10-16 10:22:54 -0500

    update home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ const Home = (props) => {
 	console.log('props in home', props)
 
 	return (
-		<div style={{display: 'flex', flexDirection: 'column', alignContent: 'center'}}>
-			<h2>Welcome to OpenWiki</h2>
+		<div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', textAlign: 'center', margin: '0'}}>
+			<h1>Welcome to OpenWiki</h1>
 			<h4>Check out the index, or sign up to add to the knowledge base!</h4>
 		</div>
 	)
